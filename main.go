c37b4077c5f3bbc7ee4bc29db7317c69f443c90f
60753515 <tomberey1@gmail.com>
2020-02-20 22:38:50 +0000

    First Commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	//Assign Port flag
+	portInput := flag.String("port", "8080", "Server init port")
+	flag.Parse()
+
+	//Set HTTP handlers
+	http.HandleFunc("/reverse", reverseHandler)
+
+	//Start HTTP server
+	err := http.ListenAndServe(":"+*portInput, nil)
+	if err != nil {
+		fmt.Printf("http server shutdown: %s", err)
+	}
+}
+
+//reverseHandler function is a HTTP handler which takes a "text" query string and returns it reversed.
+func reverseHandler(w http.ResponseWriter, r *http.Request) {
+
+	//GET method return any text in the query string, and assign.
+	textQuery := r.URL.Query().Get("text")
+
+	//Error handling: Write error code (402), if empty string returned from GET method performed on query string.
+	if textQuery == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//Call func to reverse text from GET method, and write to response body.
+	w.Write([]byte(reverse(textQuery)))
+}
+
+// Pass text string into reverse function, to reverse character order of the it.
+func reverse(text string) string {
+	var revWord string
+	for i := len(text) - 1; i > -1; i-- {
+		revWord += string(text[i]) //Concatonate each character as string.
+	}
+	return revWord
+}
